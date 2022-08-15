d3231658a16b2e0d29912b336d57d919c44db438
Egor Ermilov <egorka33rus52@gmail.com>
2022-08-15 23:47:25 +0300

    finished work

--- src/js/classes/defaults.js
+++ src/js/classes/defaults.js
@@ -27,8 +27,36 @@ class Defaults {
 			});
 		}
 	}
+
+	setServiceAlgoritmProgress() {
+		const wrap = document.querySelector('.service-algoritm');
+
+		if (wrap) {
+			const progress = wrap.querySelectorAll('.js-circle-progress');
+			const progressBar = wrap.querySelectorAll('.js-circle-progress-bar');
+			const dashArray = Number(getComputedStyle(progressBar[0]).strokeDasharray.split(', ')[0].replace('px', ''));
+			const percentPerItem = dashArray / progress.length;
+
+			progress.forEach((el, index) => {
+				progressBar[index].style.strokeDashoffset = dashArray - (index + 1) * percentPerItem;
+			});
+		}
+	}
+
+	setListenerBackButton() {
+		const button = document.querySelector('.js-back');
+
+		if (button) {
+			button.addEventListener('click', () => {
+				history.back();
+			});
+		}
+	}
+
 	init() {
 		this.changingButton();
+		this.setServiceAlgoritmProgress();
+		this.setListenerBackButton();
 	}
 }
 
